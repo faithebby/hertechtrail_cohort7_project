5e259841f85e5b4a559542a1a3cb5c548f4b4093
faithebby <adesinafaith4@gmail.com>
2023-09-15 22:26:14 +0100

    pushing forked project

---
+++ Jabula/Python/Hello Code.py
@@ -0,0 +1,8 @@
+          
+print('Enter temperature in Celsius for conversion into fahrenheit')
+print('\n')
+print('Celsius      Fahrenheit')
+print('\n')
+temp=(eval(input )      
+print(9/5*temp+32)
+print('\n')
---
+++ Jabula/Python/Hello Jabula.py
@@ -0,0 +1,4 @@
+print('Énter temperature in celsius, for converssion into Fahrenheit")
+      temp=(eval(input: )
+            print((9/5*temp+32)/n)
+            
